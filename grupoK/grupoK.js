9248a16e7e5097d0c05ade8f07a768c88e9ce80c
OskarPaul93 <paul_1993oscar@hotmail.com>
2026-06-21 18:05:47 -0500

    OJ: Informacion de los equipos grupo K Taller 1

--- grupoK/grupoK.js
+++ grupoK/grupoK.js
@@ -2,27 +2,23 @@ const teams = [
     {
         nombre: "Colombia",
         pais: "País 1",
-        ranking: 14,
         entrenador: "Nestor Lorenzo",
         imagen: "colombia.png"
     },
     {
         nombre: "Republica del Congo",
-        pais: "País 2",
         ranking: 46,
         entrenador: "Sebastien Desabre",
         imagen: "congo.png"
     },
     {
         nombre: "Portugal",
-        pais: "País 3",
         ranking: 5,
         entrenador: "Roberto Martínez",
         imagen: "portugal.png"
     },
     {
         nombre: "Uzbequiztan",
-        pais: "País 4",
         ranking: "Fuera del Top 50",
         entrenador: "Fabio Cannavaro",
         imagen: "uzbe.png"
@@ -40,7 +36,6 @@ teams.forEach(team => {
             <div class="card-content">
                 <h2>${team.nombre}</h2>
 
-                <p><strong>País:</strong> ${team.pais}</p>
 
                 <p><strong>Ranking FIFA:</strong> ${team.ranking}</p>
 
